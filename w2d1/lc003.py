6b84e2bc1e38c863e44b270d6c32fea7a72eea0e
blacksnowx <jenks.tyler@gmail.com>
2025-06-10 11:48:02 -0400

    Leetcode 3 - Learned Sliding Windows

---
+++ w2d1/lc003.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        longest = 0
+        left = 0
+        substring = set()
+        for i in range(0, len(s)):
+            while s[i] in substring:
+                substring.remove(s[left])
+                left += 1
+            substring.add(s[i])
+            if len(substring) > longest:
+                longest = len(substring)
+
+        return longest
+
+        return longest
